0eff41256715dbfd7d628ef39fea30228d34f2a8
fisbii <fisher.bi@leandev.se>
2017-09-15 18:37:53 +0800

    add hero editor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import {AppComponent} from "./app.component";
 import {HeroesComponent} from "./heroes.component";
 import {HeroService} from "./hero.service";
-import {RouterModule} from "@angular/router";
 import {DashboardComponent} from "./dashboard.component";
+import { AppRoutingModule } from './app-routing.module';
+import {HeroSearchComponent} from "./hero-search.component";
 
 
 @NgModule({
@@ -16,30 +21,15 @@ import {DashboardComponent} from "./dashboard.component";
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -4,12 +4,13 @@
 import { Component,OnInit } from '@angular/core';
 
 import { HeroService } from './hero.service';
-import {Hero} from "./Hero";
+import {Hero} from "./hero";
 
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
 
